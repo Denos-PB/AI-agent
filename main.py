a4c04f6da91d5ac456413dedbd8c57afc42600df
Denos-PB <workdmytropat@gmail.com>
2025-04-02 20:24:47 +0300

    add model for LLM

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from models.research_response import ResearchResponse
 import os
 
 load_dotenv()
 
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
 llm = ChatGoogleGenerativeAI(
-    model = "gemini",
-    google_api_key = os.getenv("GOOGLE_API_KEY"),
+    model="gemini-2.0-flash",
+    google_api_key=google_api_key,
     temperature=0.7
 )
